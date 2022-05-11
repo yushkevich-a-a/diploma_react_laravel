98992c8800a6d26211a266cde1e5c9d0974173f4
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-05-11 23:32:26 +0300

    create model hall

---
+++ app/Http/Controllers/HallController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\hall;
+use Illuminate\Http\Request;
+
+class HallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\hall  $hall
+     * @return \Illuminate\Http\Response
+     */
+    public function show(hall $hall)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\hall  $hall
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(hall $hall)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\hall  $hall
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, hall $hall)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\hall  $hall
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(hall $hall)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_11_201438_create_halls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('halls', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->float('VIP_price')->default(0);
+            $table->float('usual_price')->default(0);
+            $table->boolean('on_sale')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('halls');
+    }
+};
